b477e678160a5e8e4d7a15c0b24249a2184cddbb
Sindre Ø. Deisz <Sindre . Deisz>
2017-03-24 13:19:14 +0100

    Removed truncate Questiontable for presentation

--- js/resetCountersAndQuestions.js
+++ js/resetCountersAndQuestions.js
@@ -1,10 +1,10 @@
 var now = new Date();
-var millisTillNextHour = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds()+5, 0) - now;
+var millisTillNextHour = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds()+10, 0) - now;
 if (millisTillNextHour < 0) {
     millisTillNextHour += 3600000;
 }
 function cleanDB() {
     $(document).load('http://org.ntnu.no/tdt4140iq/iQ/php/update/updateDbCleanser.php');
-    setTimeout(cleanDB,10000);
+    setTimeout(cleanDB,100000);
 }
 setTimeout(cleanDB,millisTillNextHour);
