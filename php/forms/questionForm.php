687403c5641d7d63de788e26c35462180ccdf4dd
Gustav Fiskum <GustavFiskum@dhcp-10-22-37-205.wlan.ntnu.no>
2017-03-29 14:54:32 +0200

    Chenged the look of the textfield in questionformphp

--- php/forms/questionForm.php
+++ php/forms/questionForm.php
@@ -1,10 +1,56 @@
 
-	
-<div class="inputFormWithButton">  
-	<input type="textarea" maxlength = "200" name="question" id="question" placeholder="Write your question here...">
+<style>
+
+
+    input  {
+        width: 400%;
+        padding: 12px 20px;
+        margin: 8px 0;
+        display: inline-block;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        box-sizing: border-box;
+        text-align: center;
+    }
+
+    input:hover {
+        background-color: #45a049;
+        display: inline-block;
+    }
+
+    input[type=submit] {
+        width: 4000%;
+        background-color: #4CAF50;
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+
+
+
+
+</style>
+
+<div class="inputFormWithButton">
+
+
+    <input
+            type="text"
+            maxlength = "200"
+            name="question"
+            id="question"
+            placeholder="Write your question here...">
+
+
 </div>
 
 <!-- Denne submit knappen MÅ være en div -->
 <div id="submit" class="submitClass inputFormWithButton" type="submit" onclick='submitQuestion($("#question").val())' value="Publish" style="background-color:green; width:70px; padding: 10px;">Submit</div>
+
+
+
 
 
